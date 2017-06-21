1250089179f65e55c508a658e09e1131ae982c27
C7777 <chinuchinu0077@gmail.com>
2017-06-21 16:56:14 +0530

    Last commit

--- app/src/main/java/com/lovishsoftware/chinmay/todoapp/Pending.java
+++ app/src/main/java/com/lovishsoftware/chinmay/todoapp/Pending.java
@@ -1,9 +1,7 @@
 package com.lovishsoftware.chinmay.todoapp;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -11,10 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.util.ArrayList;
 import java.util.List;
-
 
 public class Pending extends Fragment {
 
@@ -34,7 +29,7 @@ public class Pending extends Fragment {
                              Bundle savedInstanceState) {
 
         Log.e("Pending", "Inside Pending onCreateView");
-         view = inflater.inflate(R.layout.fragment_pending, container, false);
+        view = inflater.inflate(R.layout.fragment_pending, container, false);
         pendingRecyclerView = (RecyclerView) view.findViewById(R.id.pendingrecyclerview);
         iLayoutManager = new LinearLayoutManager(getActivity());
         pendingRecyclerView.setLayoutManager(iLayoutManager);
